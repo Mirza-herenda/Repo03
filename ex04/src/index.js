1fed37b86f4b28b0535abab8b83f834ae617f9f7
Mirza <herendamirza87@gmail.com>
2022-03-30 19:24:32 +0200

    fourt task in java script-declaration variable

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main()
+{
+    //only change code below this line
+   
+    var x;
+    var y;
+    x=10;
+    y=x;
+return[x,y];
+    //only change code above this line
+}
+console.log(main());
+module.exports=main;

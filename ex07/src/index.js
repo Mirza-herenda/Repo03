ca72fdeed45b36dc10dd54a0288208f3a7355813
Mirza <herendamirza87@gmail.com>
2022-03-30 20:13:42 +0200

    eight task-camelCase variables

---
+++ ex07/src/index.js
@@ -0,0 +1,19 @@
+function main()
+{
+    //only change code below this line
+  
+    //variable declaration
+    var studyCapVar;
+    var properCamelCase;
+    var tittleCaseOver;
+
+    studyCapVar=10;
+    properCamelCase="A string";
+    tittleCaseOver=9000;
+
+    return{studyCapVar,properCamelCase,tittleCaseOver};
+      //only change code above this line
+}
+
+console.log(main());
+module.exports=main;

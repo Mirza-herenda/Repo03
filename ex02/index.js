4a348cc3325ea797c3a57f8630bc68e88cdc2b01
Mirza <herendamirza87@gmail.com>
2022-03-30 18:47:57 +0200

    second task in java script

---
+++ ex02/index.js
@@ -0,0 +1,19 @@
+function main()
+{
+    //Only change code below this line
+
+    // this is single line JavaScript
+    /*
+        document.write("this is multi-line ccmment!");
+        document.write("this is multi-line ccmment!");
+        document.write("this is multi-line ccmment!");
+        document.write("this is multi-line ccmment!");
+        document.write("this is multi-line ccmment!");
+    */
+
+        // only change code above this line
+       
+}
+
+console.log(main());
+module.exports=main;

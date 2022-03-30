a985f6c730e0f0aac8e531ea599f515d8c6372ef
Mirza <herendamirza87@gmail.com>
2022-03-30 19:51:33 +0200

    sixt task-adding operation with var

---
+++ ex06/src/index.js
@@ -0,0 +1,16 @@
+function main()
+{
+    //only change code below this line
+  var x=7;
+  var y=12;
+  var z="My name is";
+   
+  x=x+2;
+  y=y+4;
+  z=z+" john";
+  return[x,y,z];
+      //only change code above this line
+}
+
+console.log(main());
+module.exports=main;
